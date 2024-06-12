bd126366f00cdd18bbb56838bb7394fc33f9eced
Joe Roberts <roberts.josephpaul@gmail.com>
2024-06-12 10:54:50 -0400

    updated workout controller for new authz

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,6 @@
 class WorkoutsController < ApplicationController
+  include JwtHelper
+  before_action :authenticate_request
 
   def index
     logger.info @current_user.inspect
@@ -16,17 +18,19 @@ class WorkoutsController < ApplicationController
   end
 
   def create
+    puts @current_user.inspect
     @workout = Workout.new(
       date: workout_params[:date],
       time: workout_params[:time],
       notes: workout_params[:notes],
       gym_id: workout_params[:gym_id],
-      user_id: workout_params[:user_id]
+      user: @current_user
     )
     if @workout.save
       render json: @workout
     else
       render json: { error: 'Workout not saved' }
+      puts @workout.errors.full_messages
     end
   end
 
@@ -37,7 +41,6 @@ class WorkoutsController < ApplicationController
                                       :date,
                                       :time,
                                       :gym_id,
-                                      :user_id,
                                       :notes
                                     ])
   end
